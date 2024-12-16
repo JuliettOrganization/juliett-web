e6119b155089126356b1ba3248b62abd9a8c89c4
cyrille-martin <lauraluciani@macbook-air-de-laura.home>
2024-12-16 18:44:22 +0100

    Site35

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import clsx from "clsx";
+import type { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+type ClassArray = ClassValue[];
+type ClassDictionary = { [key: string]: any };
+type ClassValue = string | number | boolean | ClassArray | ClassDictionary | null | undefined;
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
