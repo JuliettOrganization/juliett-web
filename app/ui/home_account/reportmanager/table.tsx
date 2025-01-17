c9d15c64c641613788168b93dfbb7fcca1789d78
cyrille-martin <lauraluciani@macbook-air-de-laura.home>
2025-01-17 21:37:11 +0100

    Site53

--- app/ui/home_account/reportmanager/table.tsx
+++ app/ui/home_account/reportmanager/table.tsx
@@ -71,7 +71,7 @@ const ReportsTableClient: React.FC<ReportsTableClientProps> = ({ query, currentP
 
   useEffect(() => {
     fetchReports();
-  }, [accountid, query, currentPage, sortConfig, fetchReports]);
+  }, [accountid, query, currentPage, sortConfig]);
 
   const handleSort = (key: keyof Report) => {
     let direction: 'asc' | 'desc' = 'asc';
