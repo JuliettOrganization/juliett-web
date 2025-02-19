6b6122603a458ce7602e967fa8c113a35c71d629
qkponton <Quam.kponton@gmail.com>
2025-02-19 16:43:55 +0100

    Revert "Pocweb"

--- app/home_account/configuration/airlinegrouping/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AirlineGroupingPage from "@/app/home_account/configuration/airlinegrouping/ui/AirlineGroupingPage";
-
-const Page = () => {
-  return <AirlineGroupingPage />;
-};
-
-export default Page;
--- app/home_account/configuration/geogrouping/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import GeoGroupingPage from "@/app/home_account/configuration/geogrouping/ui/GeoGroupingPage";
-
-const Page = () => {
-  return <GeoGroupingPage />;
-};
-
-export default Page;
